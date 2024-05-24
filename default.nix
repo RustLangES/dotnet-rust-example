15b15712363ff4e1731122a7f2ceaa5282b7e7fc
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-05-24 01:18:47 -0400

    chore: init project

---
+++ default.nix
@@ -0,0 +1,54 @@
+{
+  system,
+  pkgs,
+  lib ? pkgs.lib,
+  stdenv ? pkgs.stdenv,
+  crane,
+  fenix,
+  ...
+}: let
+  # fenix: rustup replacement for reproducible builds
+  toolchain = fenix.${system}.fromToolchainFile {
+    file = ./rust-toolchain.toml;
+    sha256 = "sha256-opUgs6ckUQCyDxcB9Wy51pqhd0MPGHUVbwRKKPGiwZU=";
+  };
+  # crane: cargo and artifacts manager
+  craneLib = crane.${system}.overrideToolchain toolchain;
+
+  nativeBuildInputs = with pkgs; [
+    dotnet-sdk_8
+  ];
+
+  buildInputs = with pkgs; [
+    pkg-config
+    autoPatchelfHook
+  ]
+  ++ lib.optionals stdenv.buildPlatform.isDarwin [
+    pkgs.libiconv
+  ];
+
+  example = craneLib.buildPackage {
+    doCheck = false;
+    src = craneLib.cleanCargoSource (craneLib.path ./.);
+    buildPhaseCargoCommand = "HOME=$(mktemp -d fake-homeXXXX) worker-build --release --mode no-install";
+
+    installPhaseCommand = ''
+      cp -r ./build $out
+    '';
+
+    nativeBuildInputs = with pkgs; nativeBuildInputs ++ p ++ [
+    ];
+
+    inherit buildInputs;
+  };
+in
+{
+  # `nix build`
+  packages.default = example;
+
+  # `nix develop`
+  devShells.default = craneLib.devShell {
+    buildInputs = nativeBuildInputs ++ buildInputs;
+    DOTNET_ROOT = "${pkgs.dotnet-runtime_8}";
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix.url = "github:nix-community/fenix";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+  # Iterate over Arm, x86 for MacOs 🍎 and Linux 🐧
+    flake-utils.lib.eachSystem (flake-utils.lib.defaultSystems) (
+      system: let
+        bundle = import ./. {
+          inherit system flake-utils;
+          pkgs = nixpkgs.legacyPackages.${system};
+          crane = inputs.crane.lib;
+          fenix = inputs.fenix.packages;
+        };
+      in {
+        inherit (bundle) packages apps devShells;
+      }
+    );
+}
