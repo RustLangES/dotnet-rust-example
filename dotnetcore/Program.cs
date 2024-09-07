121c2061edd64af489d200238c992a95754ff0e6
Larissa <hello@hawruka.de>
2024-09-07 16:20:59 -0600

    Improve output legibility

--- dotnetcore/Program.cs
+++ dotnetcore/Program.cs
@@ -1,15 +1,23 @@
 ﻿// Disabled unused function warnings
 #pragma warning disable CS8321
 
-public abstract class Program {
-  static void Main(string[] args){
+public abstract class Program
+{
+    static void Main(string[] args)
+    {
+        var separador = String.Concat(Enumerable.Repeat("-", 20));
+
         // Ejemplo de uso de la lista
         Handlers.Lista();
+
+        Console.WriteLine(separador);
 
         // Ejemplo de uso de la función texto
         Handlers.Texto();
 
+        Console.WriteLine(separador);
+
         // Ejemplo de uso de Persona y cambiar_nacionalidad
-        Handlers.Persona();  
-  }
+        Handlers.Persona();
+    }
 }
--- rustlib/src/lib.rs
+++ rustlib/src/lib.rs
@@ -1,3 +1,5 @@
+#![warn(clippy::pedantic)]
+
 mod fruta;
 mod list;
 mod persona;
